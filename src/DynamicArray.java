007d006f25dad80ea0c35e7f7cf0b2e5aa312ccd
MeghanshBansal <meghansh.bansal_cs.aiml19@gla.ac.in>
2021-02-18 21:04:28 +0530

    Refined the code

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -15,7 +15,7 @@ class Array<E> {
         return (E) arr[i];
     }
 
-    public void set(int i, Integer e) {
+    public void set(int i, E e) {
         arr[i] = e;
     }
 
@@ -40,22 +40,21 @@ public class DynamicArray<T> {
 
     public void insert(T value) {
         if (length == capacity) {
-            System.out.println(length);
             Array<T> newarr = new Array<>(this.capacity * 2);
             for (int i = 0; i < length; i++) {
-                newarr.set(i, (Integer) arr.get(i));
+                newarr.set(i, arr.get(i));
             }
-            newarr.set(length++, (Integer) value);
+            newarr.set(length++, value);
             arr = newarr;
             this.capacity = this.capacity * 2;
         } else {
-            arr.set(length++, (Integer) value);
+            arr.set(length++, value);
         }
     }
 
     public void insert(int i, T value) {
         if (i < capacity) {
-            arr.set(i, (Integer) value);
+            arr.set(i, value);
             ++length;
         } else {
             throw new IndexOutOfBoundsException();
@@ -67,14 +66,17 @@ public class DynamicArray<T> {
             throw new IndexOutOfBoundsException();
         } else {
             for (int j = i; j < length - 1; j++) {
-                arr.set(j, (Integer) arr.get(j + 1));
+                arr.set(j, arr.get(j + 1));
             }
             --length;
         }
     }
 
     public void show() {
-        System.out.println(arr.toString());
+        for (int i = 0; i < length; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
     }
 
 }
