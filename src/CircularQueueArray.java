8ad46e6d6a5b26d86fef630798725acc400d0bd2
MeghanshBansal <meghansh.bansal_cs.aiml19@gla.ac.in>
2021-02-21 18:37:36 +0530

    Made CircularQueueArray generic

--- src/CircularQueueArray.java
+++ src/CircularQueueArray.java
@@ -1,11 +1,11 @@
-public class CircularQueueArray {
-    private final int[] arr;
+public class CircularQueueArray<T> {
+    private final Array<T> arr;
     public int size;
     public int front;
     public int rear;
 
     public CircularQueueArray(int size) {
-        this.arr = new int[size];
+        this.arr = new Array<>(size);
         this.size = size;
         this.front = this.rear = -1;
     }
@@ -18,7 +18,7 @@ public class CircularQueueArray {
         return (front == 0 && rear == size - 1) || (front == rear + 1);
     }
 
-    public void insert(int value) {
+    public void insert(T value) {
         if (isEmpty()) {
             rear++;
             front++;
@@ -29,11 +29,11 @@ public class CircularQueueArray {
         } else {
             rear++;
         }
-        arr[rear] = value;
+        arr.set(rear, value);
     }
 
-    public int pop() {
-        int temp = arr[front];
+    public T pop() {
+        T temp = arr.get(front);
         if (rear == front) {
             rear = front = 0;
         } else if (front == size - 1) {
@@ -44,21 +44,21 @@ public class CircularQueueArray {
         return temp;
     }
 
-    public int peek() {
-        return arr[front];
+    public T peek() {
+        return arr.get(front);
     }
 
     public void roll() {
-        int temp = arr[front];
-        for (int i = front; i < rear; i++) {
-            arr[i] = arr[i + 1];
+        T temp = arr.get(front);
+        for (int i = front; i < rear ; i++) {
+            arr.set(i, arr.get(i+1));
         }
-        arr[rear] = temp;
+        arr.set(rear, temp);
     }
 
     public void show() {
         for (int i = front; i < rear + 1; i++) {
-            System.out.println(arr[i]);
+            System.out.println(arr.get(i));
         }
     }
 }
